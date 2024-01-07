babba74afa6a36d13037a75cb90ed464269ef24c
aforarjun <arjunsinghrajawata1@gmail.com>
2024-01-07 19:49:26 +0530

    socket io update 02

--- src/app/(user-pages)/user/layout.tsx
+++ src/app/(user-pages)/user/layout.tsx
@@ -1,89 +1,94 @@
-"use client";
+'use client'
 
-import React, {useState, useEffect, Fragment} from "react";
-import { GiSpaceSuit } from "react-icons/gi";
-import { GoHome, GoProject } from "react-icons/go";
-import { BsChatLeft } from "react-icons/bs";
-import { MdOutlineContacts } from "react-icons/md";
-import { RiBarChartHorizontalLine } from "react-icons/ri";
-import { usePathname } from "next/navigation";
+import React, { useState, useEffect, Fragment } from 'react'
+import { GiSpaceSuit } from 'react-icons/gi'
+import { GoHome, GoProject } from 'react-icons/go'
+import { BsChatLeft } from 'react-icons/bs'
+import { MdOutlineContacts } from 'react-icons/md'
+import { RiBarChartHorizontalLine } from 'react-icons/ri'
+import { usePathname } from 'next/navigation'
 
-import styles from "@/styles/userLayout.module.scss";
-import { LoadingUser, Sidebar } from "@/components";
-import { SocketProvider } from "@/providers/socketIo";
-import { useAppSelector } from "@/redux/hook";
-import { RootState } from "@/redux/store/store";
+import styles from '@/styles/userLayout.module.scss'
+import { LoadingUser, Sidebar } from '@/components'
+import { SocketProvider } from '@/providers/socketIo'
+import { useAppSelector } from '@/redux/hook'
+import { RootState } from '@/redux/store/store'
 
-const { userLayout, container, userProfileMenu } = styles;
+const { userLayout, container, userProfileMenu } = styles
 
 const menu = [
-  {
-    name: "WebTimes",
-    Icon: <span>WT</span>,
-    link: "/",
-  },
-  {
-    name: "My Space",
-    Icon: <GiSpaceSuit size={35} />,
-    link: "/user/profile",
-  },
-  {
-    name: "Home",
-    Icon: <GoHome size={35} />,
-    link: "/user",
-  },
-  {
-    name: "My Orders",
-    Icon: <GoProject size={35} />,
-    link: "/user/orders",
-  },
-  {
-    name: "Chats",
-    Icon: <BsChatLeft size={35} />,
-    link: "/user/chats",
-  },
-  {
-    name: "Contact Us",
-    Icon: <MdOutlineContacts size={35} />,
-    link: "/contact-us",
-  },
-];
+    {
+        name: 'WebTimes',
+        Icon: <span>WT</span>,
+        link: '/',
+    },
+    {
+        name: 'My Space',
+        Icon: <GiSpaceSuit size={35} />,
+        link: '/user/profile',
+    },
+    {
+        name: 'Home',
+        Icon: <GoHome size={35} />,
+        link: '/user',
+    },
+    {
+        name: 'My Orders',
+        Icon: <GoProject size={35} />,
+        link: '/user/orders',
+    },
+    {
+        name: 'Chats',
+        Icon: <BsChatLeft size={35} />,
+        link: '/user/chats',
+    },
+    {
+        name: 'Contact Us',
+        Icon: <MdOutlineContacts size={35} />,
+        link: '/contact-us',
+    },
+]
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname();
-  const { isAuthenticated } = useAppSelector(
-    (state: RootState) => state.authUser
-  );
-  const [openMenu, setOpenMenu] = useState(false);
+    const pathname = usePathname()
+    const { isAuthenticated } = useAppSelector(
+        (state: RootState) => state.authUser
+    )
+    const [openMenu, setOpenMenu] = useState(false)
 
-  useEffect(() => { 
-    setOpenMenu(false);
-  }, [pathname]);
+    useEffect(() => {
+        setOpenMenu(false)
+    }, [pathname])
 
-  const handleOpenMenu = () => setOpenMenu(!openMenu);
+    const handleOpenMenu = () => setOpenMenu(!openMenu)
 
-  return (
-    <div className={userLayout}>
-      {!isAuthenticated ? (
-        <LoadingUser />
-      ) : (
-        <Fragment>
-          <div className={`flex ${userProfileMenu}`} onClick={handleOpenMenu}>
-            <RiBarChartHorizontalLine
-              size={22}
-              color="var(--black-color)"
-            />
-          </div>
+    return (
+        <div className={userLayout}>
+            {!isAuthenticated ? (
+                <LoadingUser />
+            ) : (
+                <Fragment>
+                    <div
+                        className={`flex ${userProfileMenu}`}
+                        onClick={handleOpenMenu}
+                    >
+                        <RiBarChartHorizontalLine
+                            size={22}
+                            color="var(--black-color)"
+                        />
+                    </div>
 
-          <Sidebar menu={menu} openMenu={openMenu} />
+                    <Sidebar menu={menu} openMenu={openMenu} />
 
-          <SocketProvider>
-            <div className={`${container} dContainer`}>{children}</div>
-          </SocketProvider>
-        </Fragment>
-      )}
-    </div>
-  );
-};
+                    <SocketProvider>
+                        <div className={`${container} dContainer`}>
+                            {children}
+                        </div>
+                    </SocketProvider>
+                </Fragment>
+            )}
+        </div>
+    )
+}
 
-export default Layout;
+export default Layout
--- src/providers/socketIo.tsx
+++ src/providers/socketIo.tsx
@@ -1,125 +1,129 @@
-"use client";
+'use client'
 
-import { createContext, useContext, useEffect, useState } from "react";
-import toast from "react-hot-toast";
-import { io as ClientIO } from "socket.io-client";
+import { createContext, useContext, useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+import { io as ClientIO } from 'socket.io-client'
 
-import { useAppDispatch } from "@/redux/hook";
-import { AddDirectMessage } from "@/redux/slices/conversationSlice";
+import { useAppDispatch } from '@/redux/hook'
+import { AddDirectMessage } from '@/redux/slices/conversationSlice'
 
 type SocketContextType = {
-  socket: any | null;
-  isConnected: boolean;
-};
+    socket: any | null
+    isConnected: boolean
+}
 
 const SocketContext = createContext<SocketContextType>({
-  socket: null,
-  isConnected: false,
-});
+    socket: null,
+    isConnected: false,
+})
 
-export const useSocket = () => useContext(SocketContext);
+export const useSocket = () => useContext(SocketContext)
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-  const [socket, setSocket] = useState<any>(null);
-  const [isConnected, setIsConnected] = useState(false);
-  const dispatch = useAppDispatch();
+    const [socket, setSocket] = useState<any>(null)
+    const [isConnected, setIsConnected] = useState(false)
+    const dispatch = useAppDispatch()
 
-  console.log(isConnected);
+    console.log(isConnected)
 
-  useEffect(() => {
-    const user_id = localStorage.getItem("user_id");
+    useEffect(() => {
+        const user_id = localStorage.getItem('user_id')
 
-    const socketInstance = ClientIO(`${process.env.NEXT_PUBLIC_SOCKET_URL}`, {
-      path: "/api/socket/io",
-      query: {
-        user_id: user_id,
-      },
-      transports: ['websocket'],
-      // addTrailingSlash: false,
-      // extraHeaders: {
-      //   'Access-Control-Allow-Origin': '*'
-      // }
-    });
+        const socketInstance = ClientIO(
+            `${process.env.NEXT_PUBLIC_SOCKET_URL}`,
+            {
+                path: '/api/socket/io',
+                query: {
+                    user_id: user_id,
+                },
+                // transports: ['websocket'],
+                addTrailingSlash: false,
+                extraHeaders: {
+                  'Access-Control-Allow-Origin': '*'
+                }
+            }
+        )
 
-    socketInstance.on("connect", () => {
-      setIsConnected(true);
-    });
-
-    socketInstance.on("disconnect", () => {
-      setIsConnected(false);
-    });
-
-    socketInstance.on("error", (error: any) => {
-      toast.error(`WebSocket error: ${error.message}`);
-      console.error("WebSocket error:", error);
-      setIsConnected(false);
-    });
-
-    socketInstance.on("connect_error", (error: any) => {
-      toast.error(`WebSocket error: ${error.message}`);
-      console.error("WebSocket error:", error);
-      setIsConnected(false);
-    });
-
-    setSocket(socketInstance);
-
-    // socket.on("audio_call_notification", (data) => {
-    //   // TODO => dispatch an action to add this in call_queue
-    //   dispatch(PushToAudioCallQueue(data));
-    // });
-
-    // socket.on("video_call_notification", (data) => {
-    //   // TODO => dispatch an action to add this in call_queue
-    //   dispatch(PushToVideoCallQueue(data));
-    // });
-
-    socketInstance.on("new_message", (data: any) => {
-      const { message, conversation_id } = data;
-
-      dispatch(
-        AddDirectMessage({
-          conversation_id: conversation_id,
-          message: {
-            id: message._id,
-            type: "msg",
-            subtype: message.type,
-            message: message.text,
-            incoming: message.to === user_id,
-            outgoing: message.from === user_id,
-          },
+        socketInstance.on('connect', () => {
+            setIsConnected(true);
+            console.log("connected!!")
         })
-      );
-    });
 
-    // socketInstance.on("start_chat", (data: any) => {
-    //   console.log(data);
-    //   // add / update to conversation list
-    //   const existing_conversation = conversations.find(
-    //     (el) => el?.id === data._id
-    //   );
-    //   if (existing_conversation) {
-    //     // update direct conversation
-    //     dispatch(UpdateDirectConversation({ conversation: data }));
-    //   } else {
-    //     // add direct conversation
-    //     dispatch(AddDirectConversation({ conversation: data }));
-    //   }
-    // });
+        socketInstance.on('disconnect', () => {
+            setIsConnected(false)
+        })
 
-    return () => {
-      if (socket?.readyState === 1) {
-        socket?.off("new_message");
-        socket.on("end", (e:any) => {
-          console.log(e)
-        });
-        socket.disconnect();
-      }
-    };
-  }, [dispatch]);
+        socketInstance.on('error', (error: any) => {
+            toast.error(`WebSocket error: ${error.message}`)
+            console.error('WebSocket error:', error)
+            setIsConnected(false)
+        })
 
-  return (
-    <SocketContext.Provider value={{ socket, isConnected }}>
-      {children}
-    </SocketContext.Provider>
-  );
-};
+        socketInstance.on('connect_error', (error: any) => {
+            toast.error(`WebSocket error: ${error.message}`)
+            console.error('WebSocket error:', error)
+            setIsConnected(false)
+        })
+
+        setSocket(socketInstance)
+
+        // socket.on("audio_call_notification", (data) => {
+        //   // TODO => dispatch an action to add this in call_queue
+        //   dispatch(PushToAudioCallQueue(data));
+        // });
+
+        // socket.on("video_call_notification", (data) => {
+        //   // TODO => dispatch an action to add this in call_queue
+        //   dispatch(PushToVideoCallQueue(data));
+        // });
+
+        socketInstance.on('new_message', (data: any) => {
+            const { message, conversation_id } = data
+
+            dispatch(
+                AddDirectMessage({
+                    conversation_id: conversation_id,
+                    message: {
+                        id: message._id,
+                        type: 'msg',
+                        subtype: message.type,
+                        message: message.text,
+                        incoming: message.to === user_id,
+                        outgoing: message.from === user_id,
+                    },
+                })
+            )
+        })
+
+        // socketInstance.on("start_chat", (data: any) => {
+        //   console.log(data);
+        //   // add / update to conversation list
+        //   const existing_conversation = conversations.find(
+        //     (el) => el?.id === data._id
+        //   );
+        //   if (existing_conversation) {
+        //     // update direct conversation
+        //     dispatch(UpdateDirectConversation({ conversation: data }));
+        //   } else {
+        //     // add direct conversation
+        //     dispatch(AddDirectConversation({ conversation: data }));
+        //   }
+        // });
+
+        return () => {
+            if (socket?.readyState === 1) {
+                socket?.off('new_message')
+                socket.on('end', (e: any) => {
+                    console.log(e)
+                })
+                socket.disconnect()
+            }
+        }
+    }, [dispatch])
+
+    return (
+        <SocketContext.Provider value={{ socket, isConnected }}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
